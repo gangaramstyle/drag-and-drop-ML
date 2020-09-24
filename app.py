3be1087bd9c833cccc8a660dfeb763770501fd4f
Vineeth Gangaram <vineeth.gangaram@gmail.com>
2020-09-24 17:03:23 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,160 @@
+import os
+import sys
+import base64
+import pydicom
+from pydicom.filebase import DicomBytesIO
+
+# Flask
+from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# SQL
+from flask_sqlalchemy import SQLAlchemy
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Some utilites
+import numpy as np
+from util import base64_to_pil
+import json
+
+# Declare a flask app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Results(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(200), nullable=False)
+    accession = db.Column(db.String(200), nullable=False)
+    result = db.Column(db.String(200), nullable=False)
+    tag = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Result %r>' % self.filename
+
+db.create_all()
+
+# You can use pretrained model from Keras
+# Check https://keras.io/applications/
+# or https://www.tensorflow.org/api_docs/python/tf/keras/applications
+
+#from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+#model = MobileNetV2(weights='imagenet')
+
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/your_model.h5'
+
+# Load your own trained model
+# model = load_model(MODEL_PATH)
+# model._make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+
+def model_predict(img, model):
+    img = img.resize((224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='tf')
+
+    preds = model.predict(x)
+    return preds
+
+table_header = [
+    'filename',
+    'accession',
+    'tag',
+    'result'
+]
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    results = Results.query.all()
+    return render_template('index.html', results=results)
+
+@app.route('/inference-status', methods=['GET'])
+def inferenceStatus():
+    if request.method == 'GET':
+        results = Results.query.all()
+        print(results)
+        table_body = []
+        for result in results:
+            row = []
+            for key in table_header:
+                row.append(getattr(result, key))
+            table_body.append(row[:])
+
+        return jsonify({'header': table_header, 'body': table_body})
+
+@app.route('/predict-test', methods=['POST'])
+def predictTest():
+    if request.method == 'POST':
+        encoded_file = request.json['file'].partition(';base64,')[2]
+        decoded_file = base64.b64decode(encoded_file)
+        dicom = pydicom.dcmread(DicomBytesIO(decoded_file))
+        result = Results(filename=request.json['title'], accession=dicom.PatientID, result="processing", tag=request.json['tag'])
+        db.session.add(result)
+        db.session.commit()
+        return "success"
+        #return render_template('index.html', results=results)
+    return 'Error'
+
+@app.route('/clear-table', methods=['POST'])
+def clearTable():
+    if request.method == 'POST':
+        Results.query.delete()
+        db.session.commit()
+        return 'Success'
+    return 'Error'
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        print(request)
+        # Get the image from post request
+        img = base64_to_pil(request.json)
+
+        # Save the image to ./uploads
+        # img.save("./uploads/image.png")
+
+        # # Make prediction
+        # preds = model_predict(img, model)
+
+        # # Process your result for human
+        # pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
+        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+
+        # result = str(pred_class[0][0][1])               # Convert to string
+        # result = result.replace('_', ' ').capitalize()
+        
+        # Serialize the result, you can add additional fields
+        # return jsonify(result=result, probability=pred_proba)
+        return jsonify(result="result", probability=0.5)
+
+    return None
+
+
+if __name__ == '__main__':
+    # app.run(port=5002, threaded=False)
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
---
+++ static/main.js
@@ -0,0 +1,278 @@
+//========================================================================
+// Drag and drop image handling
+//========================================================================
+
+var fileDrag = document.getElementById("file-drag");
+var fileSelect = document.getElementById("file-upload");
+
+// // Add event listeners
+fileDrag.addEventListener("dragover", fileDragHover, false);
+fileDrag.addEventListener("dragleave", fileDragHover, false);
+// fileDrag.addEventListener("drop", fileSelectHandler, false);
+// fileSelect.addEventListener("change", fileSelectHandler, false);
+
+// var elDrop = document.getElementById('dropzone');
+// var elItems = document.getElementById('items');
+
+// elDrop.addEventListener('dragover', function (event) {
+//     event.preventDefault();
+//     elItems.innerHTML = 0;
+// });
+
+fileDrag.addEventListener('drop', async function (event) {
+    event.preventDefault();
+    let items = await getAllFileEntries(event.dataTransfer.items);
+    console.log(items);
+});
+
+
+
+function fileDragHover(e) {
+  // prevent default behaviour
+  e.preventDefault();
+  e.stopPropagation();
+
+  fileDrag.className = e.type === "dragover" ? "upload-box dragover" : "upload-box";
+}
+
+// function fileSelectHandler(e) {
+//   // handle file selecting
+//   var files = e.target.files || e.dataTransfer.files;
+//   fileDragHover(e);
+//   for (var i = 0, f; (f = files[i]); i++) {
+//     loadFile(f);
+//   }
+// }
+
+//========================================================================
+// Web page elements for functions to use
+//========================================================================
+
+var predResult = document.getElementById("pred-result");
+var loader = document.getElementById("loader");
+
+//========================================================================
+// Main button events
+//========================================================================
+
+function submitImage() {
+  // action for the submit button
+  console.log("submit");
+
+  if (!imageDisplay.src || !imageDisplay.src.startsWith("data")) {
+    window.alert("Please select an image before submit.");
+    return;
+  }
+
+  loader.classList.remove("hidden");
+  imageDisplay.classList.add("loading");
+
+  // call the predict function of the backend
+  predictImage(imageDisplay.src);
+}
+
+
+function loadFile(file) {
+  var reader = new FileReader();
+  var tag = document.getElementById("tag").value;
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
+    submitFile(reader.result, file.name, tag);
+  };
+}
+
+//========================================================================
+// Helper functions
+//========================================================================
+
+function updateTable() {
+  fetch("/inference-status", {
+    method: "GET",
+  })
+    .then(resp => {
+      if (resp.ok)
+        resp.json().then(data => {
+          var headerData = data['header'];
+          var bodyData = data['body'];
+          console.log(bodyData);
+          if (data['body'].length != 0) {
+            // create table if table doesn't exist
+            var table = document.getElementById('table');
+            if (table === null) {
+              table = document.createElement('table');
+              table.setAttribute('id', 'table');
+              document.getElementById('content-container').append(table);
+            }
+
+            // create header if header doesn't exist
+            var header = document.getElementById('table-header');
+            if (header === null) {
+              header = document.createElement('thead');
+              header.setAttribute('id', 'table-header');
+              table.append(header);
+
+              var headerRow = document.createElement('tr');
+              header.append(headerRow);
+
+              for(var i = 0; i < headerData.length; i++) {
+                var headerElem = document.createElement('th');
+                headerElem.innerHTML = headerData[i];
+                headerRow.append(headerElem);
+              }
+            }
+
+
+            var body = document.getElementById('table-body');
+            if (body === null) {
+              body = document.createElement('tbody');
+              body.setAttribute('id', 'table-body');
+              table.append(body);
+            }
+            body.innerHTML = "";
+            //look, chill, I know  I shouldn't be using the name attribute
+            //but it made my life easier
+            for(var i = 0; i < bodyData.length; i++) {
+              //var rowId = bodyData[i]['rowID'];
+              //var updateId = bodyData[i]['updateID'];
+
+              var row = document.createElement('tr');
+              //row.setAttribute('id', rowId);
+              //row.setAttribute('name', updateId);
+              for(var j = 0; j < bodyData[i].length; j++) {
+                var cell = document.createElement('td');
+                cell.innerHTML = bodyData[i][j];
+                row.append(cell);
+              }
+              body.append(row);
+            }
+          } else {
+            console.log("remove table");
+            var table = document.getElementById('table');
+            document.getElementById('content-container').removeChild(table);
+          }
+        });
+    })
+    .catch(err => {
+      console.log("An error occured", err.message);
+      window.alert("Oops! Something went wrong.");
+    });
+}
+
+function submitFile(file, title, tag) {
+  fetch("/predict-test", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body:    JSON.stringify({title: title, file: file, tag: tag})
+  })
+    .then(resp => {
+      if (resp.ok) {
+        updateTable();
+      }
+    });
+}
+
+function download_table_as_csv(table_id) {
+  // Select rows from table_id
+  var rows = document.querySelectorAll('table#' + table_id + ' tr');
+  // Construct csv
+  var csv = [];
+  for (var i = 0; i < rows.length; i++) {
+      var row = [], cols = rows[i].querySelectorAll('td, th');
+      for (var j = 0; j < cols.length; j++) {
+          // Clean innertext to remove multiple spaces and jumpline (break csv)
+          var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
+          // Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
+          data = data.replace(/"/g, '""');
+          // Push escaped string
+          row.push('"' + data + '"');
+      }
+      csv.push(row.join(';'));
+  }
+  var csv_string = csv.join('\n');
+  // Download it
+  var filename = 'export_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
+  var link = document.createElement('a');
+  link.style.display = 'none';
+  link.setAttribute('target', '_blank');
+  link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+  link.setAttribute('download', filename);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function clearTable() {
+  fetch("/clear-table", {
+    method: "POST",
+    headers: {
+      "Content-Length": 0
+    }
+  })
+    .then(resp => {
+      if (resp.ok) {
+        updateTable();
+      }
+    });
+}
+
+
+
+
+
+// Drop handler function to get all files
+async function getAllFileEntries(dataTransferItemList) {
+  let fileEntries = [];
+  // Use BFS to traverse entire directory/file structure
+  let queue = [];
+  // Unfortunately dataTransferItemList is not iterable i.e. no forEach
+  for (let i = 0; i < dataTransferItemList.length; i++) {
+    queue.push(dataTransferItemList[i].webkitGetAsEntry());
+  }
+  while (queue.length > 0) {
+    let entry = queue.shift();
+    if (entry.isFile) {
+      fileEntries.push(entry);
+    } else if (entry.isDirectory) {
+      let reader = entry.createReader();
+      queue.push(...await readAllDirectoryEntries(reader));
+    }
+  }
+  return fileEntries;
+}
+
+// Get all the entries (files or sub-directories) in a directory by calling readEntries until it returns empty array
+async function readAllDirectoryEntries(directoryReader) {
+  let entries = [];
+  let readEntries = await readEntriesPromise(directoryReader);
+  while (readEntries.length > 0) {
+    entries.push(...readEntries);
+    readEntries = await readEntriesPromise(directoryReader);
+  }
+  return entries;
+}
+
+// Wrap readEntries in a promise to make working with readEntries easier
+async function readEntriesPromise(directoryReader) {
+  try {
+    return await new Promise((resolve, reject) => {
+      directoryReader.readEntries(resolve, reject);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+
+
+function hide(el) {
+  // hide an element
+  el.classList.add("hidden");
+}
+
+function show(el) {
+  // show an element
+  el.classList.remove("hidden");
+}
